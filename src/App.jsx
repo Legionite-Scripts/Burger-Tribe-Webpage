96e481cf77ceecc110eaeb63029d18e428c2cb59
Legionite-Scripts <samuelonwuka88@gmail.com>
2023-10-30 11:15:58 -0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Navigationbar } from "./Components/Navbar";
 import { Home } from "./Components/Home";
 import { Features } from "./Components/Features";
@@ -7,25 +8,32 @@ import { Portfolio } from "./Components/Portfolio";
 import { Contact } from "./Components/Contact";
 import { Map } from "./Components/Map";
 import { Footer } from "./Components/Footer";
-import("./assets/navbar.css");
-import("./assets/home.css");
-import("./assets/features.css");
-import("./assets/pages.css");
-import("./assets/portfolio.css");
-import("./assets/contact.css");
-import("./assets/footer.css");
+import "./assets/navbar.css";
+import "./assets/home.css";
+import "./assets/features.css";
+import "./assets/pages.css";
+import "./assets/portfolio.css";
+import "./assets/contact.css";
+import "./assets/footer.css";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navigationbar />
       <Routes>
-        {/* <Route path="/" element={<Home />}></Route> */}
-        <Route path="features" element={<Features />}></Route>
-        <Route path="pages" element={<Pages />}></Route>
-        <Route path="portfolio" element={<Portfolio />}></Route>
-        <Route path="contact" element={<Contact />}></Route>
+        <Route path="/" element={<DefaultPage />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/pages" element={<Pages />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+    </Router>
+  );
+}
+
+function DefaultPage() {
+  return (
+    <>
       <Home />
       <Features />
       <Pages />
@@ -36,4 +44,7 @@ function App() {
     </>
   );
 }
+
+
+
 export default App;
